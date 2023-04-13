794f503d60ef88ead052e57b248db6c8910ad309
Liam <liam.tessendorf@uzh.ch>
2023-04-13 18:27:11 +0200

    you can now buy tokens

--- frontend/src/components/UserPurchaseBox.js
+++ frontend/src/components/UserPurchaseBox.js
@@ -1,14 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from '@/styles/UserPurchaseBox.module.css';
+import Web3 from 'web3';
+import abi from '../contracts/abi.json';
+
 
 const UserPurchaseBox = () => {
+  const [web3, setWeb3] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [contract, setContract] = useState(null);
+  
+  const tokenPrice = 0.5; // The cost of a token in MATIC
+  const [amountInMatic, setamountInMatic] = useState('');
+  const [amountInTokens, setAmountInTokens] = useState('');
+
+  useEffect(() => {
+    if (amountInMatic !== '') {
+      setAmountInTokens(amountInMatic / tokenPrice);
+    }
+  }, [amountInMatic]);
+
+  useEffect(() => {
+    if (amountInTokens !== '') {
+      setamountInMatic(amountInTokens * tokenPrice);
+    }
+  }, [amountInTokens]);
+
+ const loadWeb3AndBlockchainData = async () => {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+      const web3 = window.web3;
+      setWeb3(web3);
+
+      // Load the current user's account
+      const accounts = await web3.eth.getAccounts();
+      setAccount(accounts[0]);
+
+      // Load the smart contract
+      const contractAddress = '0x066d7f9A63e50D29FF9853d9511D1C1ed5d21211';
+      const contract = new web3.eth.Contract(abi, contractAddress);
+      setContract(contract);
+      } else {
+      window.alert('Please install MetaMask!');
+      }
+  };
+
+  useEffect(() => {
+    loadWeb3AndBlockchainData();
+  }, []);
+
+  const buyTokens = async (amountInMatic) => {
+    if (web3 && account && contract) {
+      // Call the buyTokens function from your smart contract
+      console.log('Buying tokens...');
+      await contract.methods.buyTokens().send({ from: account, value: web3.utils.toWei(amountInMatic.toString(), 'ether') });
+    } else {
+      window.alert('Please connect to MetaMask and load the smart contract.');
+    }
+  };
+  
+
   return (
     <div>
         <div className={styles.rectangle}>
             <h1>Company Name AG</h1>
-            <input type="number" className={styles.box0} />
-            <input type="number" className={styles.box1} />
-            <button className={styles.buy}>Buy</button>
+            <input
+              type="number"
+              value={amountInMatic}
+              onChange={(e) => setamountInMatic(e.target.value)}
+              className={styles.box0}
+            />
+            <img src="/CurSelector.svg" alt="Logo" className={styles.CurSelector0} />
+            <input
+              type="number"
+              value={amountInTokens}
+              onChange={(e) => setAmountInTokens(e.target.value)}
+              className={styles.box1}
+            />
+            <img src="/CurSelector1.svg" alt="Logo" className={styles.CurSelector1} />
+            <button onClick={() => buyTokens(amountInMatic)} className={styles.buy}>Buy</button>
         </div>
     </div>
   );
