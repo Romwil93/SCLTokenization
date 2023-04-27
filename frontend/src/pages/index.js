697d7efafc00b457a2d039e2ef9d0bb89aa7e316
Liam <liam.tessendorf@uzh.ch>
2023-04-27 16:49:38 +0200

    redeploying

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,7 +3,6 @@ import Header from '../components/Header';
 import UserPurchaseBox from '../components/UserPurchaseBox';
 import Link from 'next/link';
 
-
 export default function Home() {
   return (
     <>
