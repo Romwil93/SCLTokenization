fba18f2f2871cea45ad22e54e5df7dba0bb968f4
Liam <94056511+liamti5@users.noreply.github.com>
2023-05-05 19:00:00 +0200

    Update firebase.js

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -9,14 +9,7 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDL6nSqG0f6a50tPqSHmv8knjFONzu4AEw",
-  authDomain: "scltokenization.firebaseapp.com",
-  projectId: "scltokenization",
-  storageBucket: "scltokenization.appspot.com",
-  messagingSenderId: "41458544642",
-  appId: "1:41458544642:web:a6a0bca2e4b3a9d0f6b71b",
-  measurementId: "G-9M5TEJTQSM",
-  databaseURL: "https://scltokenization-default-rtdb.europe-west1.firebasedatabase.app/",  
+
 };
 
 // Initialize Firebase
