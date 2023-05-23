786b89e9fe9d462d5c4210728a94b8abb227b0ef
Liam <liam.tessendorf@uzh.ch>
2023-05-23 15:12:36 +0200

    added icon

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import AppBar from '../components/AppBar';
-import UserPurchaseBox from '../components/UserPurchaseBox';
-import Link from 'next/link';
 
 export default function Home() {
   return (
