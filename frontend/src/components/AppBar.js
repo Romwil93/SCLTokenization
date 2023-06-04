1743dc768bad5c2710f0e5fbe2d79c634e97ce05
Liam <liam.tessendorf@uzh.ch>
2023-06-04 11:22:00 +0200

    removed white box on address field

--- frontend/src/components/AppBar.js
+++ frontend/src/components/AppBar.js
@@ -48,94 +48,72 @@ function ResponsiveAppBar() {
 
     return (
         <AppBar position="static" color="transparent">
-        <Container maxWidth="xl">
-            <Toolbar disableGutters>
-                <img
-                    src="/SCLLogo.svg" 
-                    alt="logo"
-                    className={styles.logo}
-                />
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-                >
-                <MenuIcon sx={{ color: "white"}} />
-                </IconButton>
-                <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{
-                    display: { xs: 'block', md: 'none' }
-                }}
-                >
-                {pages.map((page) => (
-                    <Link key={page} href={`/${page.toLowerCase()}`} passHref>
-                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                        <Typography textAlign="center" sx={{color: 'black', textDecoration: 'none' }}>   {page}</Typography>
-                        </MenuItem>
-                    </Link>
-                ))}
-                </Menu>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {pages.map((page) => (
-                    <Link key={page} href={`/${page.toLowerCase()}`} passHref>
-                    <Button
-                        onClick={handleCloseNavMenu}
-                        sx={{ my: 2, color: 'white', display: 'block' }}
-                    >
-                        {page}
-                    </Button>
-                    </Link>
-                ))}
-            </Box>
-            <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Typography sx={{ color: 'white' }}>{truncateAddress(ethereumAddress)}</Typography>                
-                </IconButton>
-                </Tooltip>
-                <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-                >
-                {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                        <Typography textAlign="center" sx={{ textDecoration: 'none' }}>{setting}</Typography>
-                    </MenuItem>
-                ))}
-                </Menu>
-            </Box>
-            </Toolbar>
-        </Container>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <img
+                        src="/SCLLogo.svg" 
+                        alt="logo"
+                        className={styles.logo}
+                    />
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                        size="large"
+                        aria-label="account of current user"
+                        aria-controls="menu-appbar"
+                        aria-haspopup="true"
+                        onClick={handleOpenNavMenu}
+                        color="inherit"
+                        >
+                        <MenuIcon sx={{ color: "white"}} />
+                        </IconButton>
+                        <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorElNav}
+                        anchorOrigin={{
+                            vertical: 'bottom',
+                            horizontal: 'left',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'left',
+                        }}
+                        open={Boolean(anchorElNav)}
+                        onClose={handleCloseNavMenu}
+                        sx={{
+                            display: { xs: 'block', md: 'none' }
+                        }}
+                        >
+                        {pages.map((page) => (
+                            <Link key={page} href={`/${page.toLowerCase()}`} passHref>
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                <Typography textAlign="center" sx={{color: 'black', textDecoration: 'none' }}>   
+                                    {page}
+                                </Typography>
+                                </MenuItem>
+                            </Link>
+                        ))}
+                        </Menu>
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Link key={page} href={`/${page.toLowerCase()}`} passHref>
+                            <Button
+                                onClick={handleCloseNavMenu}
+                                sx={{ my: 2, color: 'white', display: 'block' }}
+                            >
+                                {page}
+                            </Button>
+                            </Link>
+                        ))}
+                    </Box>
+                    <Box sx={{ flexGrow: 0 }}>                        
+                        <IconButton sx={{ p: 0 }}>
+                            <Typography sx={{ color: 'white' }}>{truncateAddress(ethereumAddress)}</Typography>                
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </Container>
         </AppBar>
     );
 }
